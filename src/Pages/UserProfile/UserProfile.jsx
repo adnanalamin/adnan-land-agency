6ec26ace05e6f7d3507ae4da0c2c8da928aa8c9b
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-04-14 01:42:03 +0600

    Added User Profile Route and Show User Details

--- src/Pages/UserProfile/UserProfile.jsx
+++ src/Pages/UserProfile/UserProfile.jsx
@@ -4,9 +4,16 @@ import { AuthContext } from "../../Provider/AuthProvider";
 const UserProfile = () => {
     const {user} = useContext(AuthContext)
     return (
-        <div>
-            <p>{user.displayName}</p>
-            <img src={user.photoURL} alt="ffff" />
+        <div className="mt-16 border w-2/5 mx-auto text-center shadow-[#5F8A85] p-9 rounded-2xl shadow-2xl">
+            
+            <div className="avatar">
+            <div className="w-64 rounded-full">
+            <img className="" src={user.photoURL} alt="ffff" />
+            </div>
+            </div>
+            <p className="font popi text-4xl text-[#323E3D] font-extrabold mt-5">{user.displayName}</p>
+            <p className="font popi text-2xl text-[#323E3D] font-extrabold mt-5">{user.email}</p>
+            
         </div>
     );
 };
